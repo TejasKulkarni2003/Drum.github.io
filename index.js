e272be4df5b0fbe44e26a68bca71245b64773f87
TejasKulkarni2003 <104378810+TejasKulkarni2003@users.noreply.github.com>
2022-12-23 00:12:00 +0530

    Path 2

--- index.js
+++ index.js
@@ -21,31 +21,31 @@ document.addEventListener("keydown", function(event){
 
 function drumPress(z){
     if(z === "w"){
-        var audio = new Audio("../sounds/crash.mp3");
+        var audio = new Audio(" /Drum.github.io/sounds/crash.mp3");
          audio.play();
     }
     else if(z === "a"){
-        var audio = new Audio("../sounds/kick-bass.mp3");
+        var audio = new Audio(" /Drum.github.io//sounds/kick-bass.mp3");
          audio.play();
     }
     else if(z === "s"){
-        var audio = new Audio("../sounds/snare.mp3");
+        var audio = new Audio(" /Drum.github.io//sounds/snare.mp3");
          audio.play();
     }
     else if(z === "d"){
-        var audio = new Audio("../sounds/tom-1.mp3");
+        var audio = new Audio(" /Drum.github.io//sounds/tom-1.mp3");
          audio.play();
     }
     else if(z === "j"){
-        var audio = new Audio("../sounds/tom-2.mp3");
+        var audio = new Audio(" /Drum.github.io//sounds/tom-2.mp3");
          audio.play();
     }
     else if(z === "k"){
-        var audio = new Audio("../sounds/tom-3.mp3");
+        var audio = new Audio(" /Drum.github.io//sounds/tom-3.mp3");
          audio.play();
     }
     else if(z === "l"){
-        var audio = new Audio("../sounds/tom-4.mp3");
+        var audio = new Audio(" /Drum.github.io//sounds/tom-4.mp3");
          audio.play();
     }
 }
